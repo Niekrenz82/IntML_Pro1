b265e4c0868d860cf036647ba05b7411e7bc6273
PhillipHovgesen <96547789+PhillipHovgesen@users.noreply.github.com>
2025-05-15 20:07:47 +0200

    New and final train_predict_submit

--- train_predict_submit-1.r
+++ train_predict_submit-1.r
@@ -1,7 +1,7 @@
 library(data.table)
 
 # Modify this variable to your group name, which is formatted as FirstName1_LastName1-FirstName2_LastName2-...
-GROUP_NAME <- "Kasper_Jensen-Jakob_Nielsen"
+GROUP_NAME <- "Phillip_Hovgesen-Emil_Sükrüoglu-Emil_Knudsen-Christian_Niekrenz"
 
 # Define the train function, assume that X is a data.table and y is a vector of responses of the same length as the number of rows in X
 train_model <- function(X, y) {
@@ -10,22 +10,128 @@ train_model <- function(X, y) {
 
   # ---- MODIFY THIS SECTION ----
   library(mlr3verse)
-  library(mlr3)
-  library(mlr3learners)
-  library(mlr3tuning)
-  library(mlr3pipelines)
-
+  library(mlr3extralearners)
+  library(dplyr)
+  library(lubridate)
+  
+  # Functions for data transformation
+  reference_date <- as.Date("2015-01-01")
+  
+  normalize_date_to_days <- function(date_vector) {
+    as.numeric(date_vector - reference_date)
+  }
+  
+  format_data <- function(data) {
+    # Convert categorical variables to factors
+    data <- data %>% mutate(
+      ID = as.numeric(ID),
+      Date_start_contract = dmy(Date_start_contract),
+      Date_last_renewal = dmy(Date_last_renewal),
+      Date_next_renewal = dmy(Date_next_renewal),
+      Distribution_channel = factor(
+        Distribution_channel,
+        levels = c(0, 1),
+        labels = c("Agent", "Broker")
+      ),
+      Date_birth = dmy(Date_birth),
+      Date_driving_licence = dmy(Date_driving_licence),
+      Seniority = as.numeric(Seniority),
+      Policies_in_force = as.numeric(Policies_in_force),
+      Max_policies = as.numeric(Max_policies),
+      Max_products = as.numeric(Max_products),
+      Lapse = as.numeric(Lapse),
+      Date_lapse = dmy(Date_lapse),
+      Payment = factor(
+        Payment,
+        levels = c(0, 1),
+        labels = c("Annual", "Half-yearly")
+      ),
+      Premium = as.numeric(Premium),
+      N_claims_year = as.numeric(N_claims_year),
+      N_claims_history = as.numeric(N_claims_history),
+      R_Claims_history = as.numeric(R_Claims_history),
+      Type_risk = factor(
+        Type_risk,
+        levels = c(1, 2, 3, 4),
+        labels = c(
+          "Motorbikes",
+          "Vans",
+          "Passenger cars",
+          "Agricultural vehicles"
+        )
+      ),
+      Area = factor(
+        Area,
+        levels = c(0, 1),
+        labels = c("Rural", "Urban")
+      ),
+      Second_driver = factor(
+        Second_driver,
+        levels = c(0, 1),
+        labels = c("Single", "Multiple")
+      ),
+      Year_matriculation = as.numeric(Year_matriculation),
+      Power = as.numeric(Power),
+      Cylinder_capacity = as.numeric(Cylinder_capacity),
+      Value_vehicle = as.numeric(Value_vehicle),
+      N_doors = as.numeric(N_doors),
+      Type_fuel = factor(
+        Type_fuel,
+        levels = c("P", "D"),
+        labels = c("Petrol", "Diesel")
+      ),
+      Length = as.numeric(Length),
+      Weight = as.numeric(Weight)
+    ) %>%
+      mutate(
+        Age = year(as.period(
+          interval(start = Date_birth, end = Date_last_renewal)
+        )),
+        Age_driving_licence = year(as.period(
+          interval(start = Date_driving_licence, end = Date_last_renewal)
+        )),
+        Vehicle_age = as.numeric(format(Date_last_renewal, "%Y")) - as.numeric(Year_matriculation),
+        Exposure = as.numeric(
+          if_else(
+            is.na(Date_lapse) |
+              Date_next_renewal < Date_lapse,
+            Date_next_renewal,
+            Date_lapse
+          ) - Date_last_renewal
+        ) / 365,
+      ) %>% mutate(Exposure = if_else(Exposure > 1, 1, Exposure)) %>%
+      filter(Exposure > 0) %>%
+      select(-c(N_claims_year, Date_next_renewal, Date_lapse, Lapse, Year_matriculation, Date_driving_licence, Date_birth, ID))
+    
+    date_cols <- data %>%
+      select(where(is.Date)) %>%
+      colnames()
+    
+    data <- data %>%
+      mutate(across(all_of(date_cols), ~ year(.x), .names = "{.col}_year")) %>%
+      mutate(across(all_of(date_cols), ~ month(.x), .names = "{.col}_month")) %>%
+      mutate(across(all_of(date_cols), ~ day(.x), .names = "{.col}_day")) %>%
+      mutate(across(all_of(date_cols), normalize_date_to_days))
+    
+    return(data)
+  }
+  
+  X <- format_data(X)
+  
   task <- as_task_regr(data.table(X, y = y), target = "y")
-
-  # Select only numeric columns for training
-  select_num <- po("select", selector = selector_type(c("numeric", "integer")))
-
-  learner <- select_num %>>%
-    lrn("regr.xgboost",
-      nrounds = to_tune(10, 100),
-      max_depth = to_tune(1, 10),
-      eta = to_tune(0.01, 0.1)
-    )
+  
+  learner <- lrn(
+    "regr.lightgbm",
+    id = "lightgbm",
+    objective = "tweedie",
+    num_iterations = to_tune(100,5000),
+    learning_rate = to_tune(0.005, 0.1),
+    num_leaves = to_tune(10, 512),
+    max_depth = to_tune(3, 15),
+    feature_fraction = to_tune(0.6, 1.0),
+    bagging_fraction = to_tune(0.6, 1.0),
+    min_data_in_leaf = to_tune(1, 100)
+  )
 
 
   at <- auto_tuner(
@@ -33,7 +139,7 @@ train_model <- function(X, y) {
     learner = learner,
     resampling = rsmp("cv", folds = 5),
     measure = msr("regr.mse"),
-    term_evals = 10
+    term_evals = 100
   )
   at$train(task)
   model <- at$learner
@@ -44,6 +150,7 @@ train_model <- function(X, y) {
     # assume that test_X is the same format as X
     # call your model here, e.g. in mlr3 you can use the predict function
     # ---- MODIFY THIS SECTION ----
+    test_X <- format_data(test_X)
     preds <- model$predict_newdata(test_X)
     return(preds$response)
     # ---- END OF MODIFY THIS SECTION ----
@@ -55,7 +162,7 @@ train_model <- function(X, y) {
 # run the following function to check if your train_model function is working
 # do not modify this function
 test_model_passes <- function() {
-  data <- fread("Motor vehicle insurance data.csv", sep = ";")
+  data <- fread("data/Motor vehicle insurance data.csv", sep = ";")
 
   X <- data[, -c("Cost_claims_year")]
   y <- data$Cost_claims_year
@@ -80,4 +187,4 @@ test_model_passes <- function() {
 }
 
 # uncomment this line to test your model
-# test_model_passes()
+test_model_passes()
